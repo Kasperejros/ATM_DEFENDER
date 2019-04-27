fe774b73f8364173a52df6f3ba2f81452b06eb35
Kasperejros <krajewski.kacper@gmail.com>
2019-04-27 11:14:55 +0200

    init

---
+++ desktop/src/com/atm/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.atm.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.atm.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
